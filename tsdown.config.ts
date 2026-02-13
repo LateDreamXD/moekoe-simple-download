fc358f7f423ae9cecd736173dc6d3148c226378b
LateDreamXD <127290936+LateDreamXD@users.noreply.github.com>
2026-02-13 20:40:12 +0800

    chore: remove popup

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,17 +1,19 @@
 import { defineConfig } from 'tsdown';
+import vue from 'unplugin-vue/rolldown';
+import raw from 'unplugin-raw/rolldown';
+import scss from 'rollup-plugin-scss';
 
-export default defineConfig([{
-	entry: {
-		content: 'src/content/index.ts'
-	},
+export default defineConfig({
+	define: { isProd: JSON.stringify(true) },
+	noExternal: ['vue'],
+	entry: { content: 'src/content/index.ts' },
+	plugins: [vue(), raw(), scss({
+		fileName: 'content.css',
+		sass: await import('sass-embedded'),
+		outputStyle: 'compressed'
+	})],
+	platform: 'browser',
 	minify: true,
-	dts: false,
-	format: 'iife'
-}, {
-	entry: {
-		popup: 'src/popup/index.ts'
-	},
-	minify: true,
-	dts: false,
-	format: 'iife'
-}]);
+	format: 'iife',
+	tsconfig: 'tsconfig.app.json'
+});
