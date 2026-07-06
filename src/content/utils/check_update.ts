88b5a5dadee1e8de24a820b41811b19e54115711
latedream <xiaozhisans@qq.com>
2026-07-06 16:34:36 +0800

    fix(check_update): sync new version rules

--- src/content/utils/check_update.ts
+++ src/content/utils/check_update.ts
@@ -1,8 +1,9 @@
 export const checkUpdate = async (version: string) => {
 	const res = await fetch('https://github.com/LateDreamXD/moekoe-simple-download/releases/latest',
 		{ redirect: 'follow', mode: 'no-cors' });
-	const latestVersion = res.url.split('/').pop()?.replace('v', '') || '0.0.0';
+	const latestVersion = res.url.split('/').pop()?.replace('v', '').replace('-', '.') || '0.0.0';
 	const current = version.split('.'), latest = latestVersion.split('.');
+	if(latestVersion.length > current.length) return latestVersion;
 	for(let i = 0; i < current.length; i++) {
 		if(Number(current[i]) > Number(latest[i])) return false;
 		if(Number(current[i]) < Number(latest[i])) return latestVersion;
